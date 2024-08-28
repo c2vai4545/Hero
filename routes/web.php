09468c7d31e4c11f707fc71b42ace1be0e204928
Carlos Nuñez <c2vai2003@me.com>
2024-08-28 16:40:42 -0300

    Se inicializa el proyecto Hero Legend con Laravel y se realizan las primeras configuraciones aprendiendo Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Metadata\Api\HookMethods;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home/{name}', function ($name) {
+    return view('home', ['name' => $name]);
+})->where('name','[A-Za-z]+');
+/**
+ * El ->where('name', '[A-Za-z]+') indica que 
+ * solamente aceptara Letras, sin importar si tienen numeros o no pero tiene 
+ * que tener letras sino lo rechaza
+ */
